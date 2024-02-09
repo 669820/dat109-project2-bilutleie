d8f663b3b8a4f53b53d77450d46684dc2328ab19
Ole Martin Amundsen <omamundsen42@gmail.com>
2024-02-09 21:34:10 +0100

    Har lagt til utleie - ole

---
+++ src/main/java/no/hvl/dat109/bilutleie/Utleie.java
@@ -0,0 +1,35 @@
+package no.hvl.dat109.bilutleie;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class Utleie {
+
+	private int utleieID;
+	private Reservasjon reservasjon;
+	private int startKilometer;
+	private int sluttKilometer;
+	private LocalDate returDato;
+	private int pris;
+	
+	//Teller for identifikator
+	private static int teller = 1;
+	
+	/*
+	 * Konstruktør
+	 */
+	public Utleie(Reservasjon reservasjon, int startKilometer, int pris) {
+		this.utleieID = teller++;
+		this.reservasjon = reservasjon;
+		this.startKilometer = startKilometer;
+		this.pris = pris;
+	}
+	
+	public void registrerRetur(int utleieID) {
+		returDato = LocalDate.now();
+		//Fikse logikk for å avslutte reservasjon
+	}
+	
+	 
+	
+}

64749d81afec7fde688dd2d055b283aa65434952
Justin <justinjustin.roland@gmail.com>
2024-02-12 20:17:25 +0100

    adda test mappe fordi Erlend ikke kan Eclipse. Gaga Gugu

---
+++ src/test/java/bilutleietest/KundeTest.java
@@ -0,0 +1,14 @@
+package bilutleietest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KundeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/bilutleietest/ReservasjonTest.java
@@ -0,0 +1,14 @@
+package bilutleietest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReservasjonTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
